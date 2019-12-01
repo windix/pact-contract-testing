efe517efe5a072eed949aaaafab8ed5dd3d42923
O' Connor <nialloc9@gmail.com>
2019-12-01 18:46:36 +0000

    feat: added pact consumer tests and federated application

--- consumer.js
+++ consumer.js
@@ -1,26 +1,6 @@
 const axios = require('axios');
 
-// const options = {
-//   Accept: 'api_version=2',
-//   headers: { 'Content-Type': 'application/graphql' }
-// }
-
 const url = `http://localhost:5000`;
-// const query: `
-// query PostsForAuthor {
-//   author(id: 1) {
-//     firstName
-//       posts {
-//         title
-//         votes
-//       }
-//     }
-//   }
-// `
-
-// const hello1Query = () => console.log("here") || axios.post(url, {
-//     query
-// }, options);
 
 const hello1Query = () => axios({
   url,
