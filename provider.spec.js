0e2cf6c26b3b9fdc4b3ce2f979504f5b617a1427
O' Connor <nialloc9@gmail.com>
2019-12-01 19:03:03 +0000

    feat: added provider tests

--- provider.spec.js
+++ provider.spec.js
@@ -1,16 +1,15 @@
 const { Verifier } = require('@pact-foundation/pact');
 
-const { PACT_BROKER_URL, PACT_BROKER_USERNAME, PACT_BROKER_PASSWORD } = process.env;
+const { PACT_BROKER_TOKEN, PACT_BROKER_URL } = process.env;
 
 describe('Pact Verification', () => {
   it('validates the contract provided by the consumer', () => {
     const opts = {
-      provider: 'Provider',
+      provider: 'GraphQLProvider',
       providerBaseUrl: 'http://localhost:4000',
       pactBrokerUrl: PACT_BROKER_URL,
-      tags: ['production', 'test'],
-      pactBrokerUsername: PACT_BROKER_USERNAME,
-      pactBrokerPassword: PACT_BROKER_PASSWORD,
+      pactBrokerToken: PACT_BROKER_TOKEN,
+      tags: ['test'],
       publishVerificationResult: true,
       providerVersion: '1.0.0',
       logLevel: 'DEBUG',
--- publish.js
+++ publish.js
@@ -1,13 +1,13 @@
 const pact = require("@pact-foundation/pact-node");
 const path = require("path");
 
-const { PACT_BROKER_TOKEN, PACT_URL } = process.env;
+const { PACT_BROKER_TOKEN, PACT_BROKER_URL } = process.env;
 
 const opts = {
   pactFilesOrDirs: [
     path.resolve(__dirname, "pacts/graphqlconsumer-graphqlprovider.json"),
   ],
-  pactBroker: PACT_URL,
+  pactBroker: PACT_BROKER_URL,
   pactBrokerToken: PACT_BROKER_TOKEN,
   tags: ["test"],
   consumerVersion:
@@ -19,7 +19,7 @@ pact
   .then(() => {
     console.log("Pact contract publishing complete!")
     console.log("")
-    console.log(`Head over to ${PACT_URL} and login to see your contracts.`)
+    console.log(`Head over to ${PACT_BROKER_URL} and login to see your contracts.`)
   })
   .catch(e => {
     console.log("Pact contract publishing failed: ", e)
